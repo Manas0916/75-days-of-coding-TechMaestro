d3880abc1e373fce8dda03bdd9809f38a531ccb0
Manas Bajpai <manas.bajpai16@gmail.com>
2022-04-23 21:33:46 +0530

    day-31 4/4 done

---
+++ day-31/Lowest Common Ancestor of a Binary Tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution 
+{
+    public TreeNode lowestCommonAncestor(TreeNode r, TreeNode p, TreeNode q) 
+    {
+        if(r == null)
+            return null;
+        
+        if(r.val == p.val || r.val == q.val)
+            return r;
+        
+        TreeNode lca1 = lowestCommonAncestor(r.left , p , q);
+        TreeNode lca2 = lowestCommonAncestor(r.right , p , q);
+        
+        if(lca1 != null && lca2 != null)
+            return r;
+        
+        return (lca1 != null) ? lca1 : lca2;
+    }
+}
